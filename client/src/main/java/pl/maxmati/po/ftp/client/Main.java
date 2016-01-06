5b8fe003b1ab398f9e4a8dbd4d4ac31caf60f782
Mateusz "maxmati" Nowoty?ski <admin@maxmati.pl>
2016-01-06 19:26:53 +0100

    Initial commit.

---
+++ client/src/main/java/pl/maxmati/po/ftp/client/Main.java
@@ -0,0 +1,10 @@
+package pl.maxmati.po.ftp.client;
+
+/**
+ * Created by maxmati on 1/6/16
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Client");
+    }
+}
